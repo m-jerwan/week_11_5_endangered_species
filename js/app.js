119046f011435e01bd253a02fc6ba06c411556ae
Marcin Jerwan <m.jerwan@gmail.com>
2018-09-09 19:54:35 +0100

    refactor stage2: helpers tested

--- js/app.js
+++ js/app.js
@@ -5,57 +5,72 @@ document.addEventListener('DOMContentLoaded', () => {
     theForm.addEventListener('submit', handleFormSubmition);
 })
 
+
+//MAIN ENGINE:
+const handleFormSubmition = function (event) {
+    event.preventDefault();
+    const rawData = extractRawData()
+    const preparedData = prepDataIntoHtml(rawData);
+    appendElements(preparedData);
+    this.reset();
+}
+
+
+//===HELPERS:
+
 const extractRawData = function(){
     const thisFormSubmition = event.target;
     const rawData = {
-        commonName: thisFormSubmition.common_name.value,
-        scientificName: thisFormSubmition.scientific_name.value,
+        common_name: thisFormSubmition.common_name.value,
+        scientific_name: thisFormSubmition.scientific_name.value,
         population: thisFormSubmition.population.value,
         habitat: thisFormSubmition.habitat.value,
-        conservationStatus: thisFormSubmition.conservation_status.value
+        conservation_status: thisFormSubmition.conservation_status.value
     }
     return rawData;
 }
 
+const prepDataIntoHtml = function(rawData){
 
-const handleFormSubmition = function (event) {
-    event.preventDefault();
-    const rawData = extractRawData()
+    let preparingELements = {
+     resultDiv: document.createElement('div'),
+     resultH: document.createElement('h3'),
+     resultUl: document.createElement('ul'),
+     resultLiSciName: document.createElement('li'),
+     resultLiPopulation: document.createElement('li'),
+     resultLiHabitat: document.createElement('li'),
+     resultLiStatus: document.createElement('li')
+    }
 
-    prepDataIntoHtml(rawData);
-    // let resultDiv = document.createElement('div');
-    // let resultH = document.createElement('h3');
-    // let resultUl = document.createElement('ul');
-    // let resultLiSciName = document.createElement('li');
-    // let resultLiPopulation = document.createElement('li');
-    // let resultLiHabitat = document.createElement('li');
-    // let resultLiStatus = document.createElement('li');
-
-    // resultH.textContent = commonName;
-    // resultLiSciName.textContent = `Scientific name: ${scientificName}`;
-    // resultLiPopulation.textContent = `Population: ${population}`;
-    // resultLiHabitat.textContent = `Habitat: ${habitat}`;
-    // resultLiStatus.textContent = `Conservation status: ${conservationStatus}`;
-
-    // if (conservationStatus === "Extinct" || conservationStatus === "Extinct in the wild" || conservationStatus === "Critically endangered") {
-    //     console.dir(conservationStatus);
-    //     resultLiStatus.className = "red"
-    // } else if (conservationStatus === "Endangered" || conservationStatus === "Vulnerable" || conservationStatus === "Near threatened") {
-    //     resultLiStatus.className = "yellow"
-    // }else{
-    //     resultLiStatus.className = "green"
-    // }
+    preparingELements.resultH.textContent = `${rawData.common_name}`;
+    preparingELements.resultLiSciName.textContent = `Scientific name: ${rawData.scientific_name}`;
+    preparingELements.resultLiPopulation.textContent = `Population: ${rawData.population}`;
+    preparingELements.resultLiHabitat.textContent = `Habitat: ${rawData.habitat}`;
+    preparingELements.resultLiStatus.textContent = `Conservation status: ${rawData.conservation_status}`;
 
 
+    if (rawData.conservation_status === "Extinct" || rawData.conservation_status === "Extinct in the wild" || rawData.conservation_status === "Critically endangered") {
+        preparingELements.resultLiStatus.className = "red"
+    } else if (rawData.conservation_status === "Endangered" || rawData.conservation_status === "Vulnerable" || rawData.conservation_status === "Near threatened") {
+        preparingELements.resultLiStatus.className = "yellow"
+    }else{
+        preparingELements.resultLiStatus.className = "green"
+    }
 
-    // resultDiv.appendChild(resultH);
-    // resultUl.appendChild(resultLiSciName);
-    // resultUl.appendChild(resultLiPopulation);
-    // resultUl.appendChild(resultLiHabitat);
-    // resultUl.appendChild(resultLiStatus);
-    // resultDiv.appendChild(resultUl);
-
-    // const listToInputResults = document.querySelector('.list');
-    // listToInputResults.appendChild(resultDiv);
-    // this.reset();
+    return preparingELements;
 }
+
+
+
+const appendElements = function (data){
+    data.resultDiv.appendChild(data.resultH);
+    data.resultUl.appendChild(data.resultLiSciName);
+    data.resultUl.appendChild(data.resultLiPopulation);
+    data.resultUl.appendChild(data.resultLiHabitat);
+    data.resultUl.appendChild(data.resultLiStatus);
+    data.resultDiv.appendChild(data.resultUl);
+
+    const listToInputResults = document.querySelector('.list');
+    listToInputResults.appendChild(data.resultDiv);
+}
+
