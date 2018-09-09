da9b8dd1551c870637eb4e0b3d7558e5e2cd78d9
Marcin Jerwan <m.jerwan@gmail.com>
2018-09-09 19:10:07 +0100

    refactor stage1: extracting raw data TESTED

--- js/app.js
+++ js/app.js
@@ -5,49 +5,57 @@ document.addEventListener('DOMContentLoaded', () => {
     theForm.addEventListener('submit', handleFormSubmition);
 })
 
+const extractRawData = function(){
+    const thisFormSubmition = event.target;
+    const rawData = {
+        commonName: thisFormSubmition.common_name.value,
+        scientificName: thisFormSubmition.scientific_name.value,
+        population: thisFormSubmition.population.value,
+        habitat: thisFormSubmition.habitat.value,
+        conservationStatus: thisFormSubmition.conservation_status.value
+    }
+    return rawData;
+}
+
+
 const handleFormSubmition = function (event) {
     event.preventDefault();
-    const thisFormSubmition = event.target;
+    const rawData = extractRawData()
 
-    const commonName = thisFormSubmition.common_name.value;
-    const scientificName = thisFormSubmition.scientific_name.value;
-    const population = thisFormSubmition.population.value;
-    const habitat = thisFormSubmition.habitat.value;
-    const conservationStatus = thisFormSubmition.conservation_status.value;
+    prepDataIntoHtml(rawData);
+    // let resultDiv = document.createElement('div');
+    // let resultH = document.createElement('h3');
+    // let resultUl = document.createElement('ul');
+    // let resultLiSciName = document.createElement('li');
+    // let resultLiPopulation = document.createElement('li');
+    // let resultLiHabitat = document.createElement('li');
+    // let resultLiStatus = document.createElement('li');
 
-    let resultDiv = document.createElement('div');
-    let resultH = document.createElement('h3');
-    let resultUl = document.createElement('ul');
-    let resultLiSciName = document.createElement('li');
-    let resultLiPopulation = document.createElement('li');
-    let resultLiHabitat = document.createElement('li');
-    let resultLiStatus = document.createElement('li');
+    // resultH.textContent = commonName;
+    // resultLiSciName.textContent = `Scientific name: ${scientificName}`;
+    // resultLiPopulation.textContent = `Population: ${population}`;
+    // resultLiHabitat.textContent = `Habitat: ${habitat}`;
+    // resultLiStatus.textContent = `Conservation status: ${conservationStatus}`;
 
-    resultH.textContent = commonName;
-    resultLiSciName.textContent = `Scientific name: ${scientificName}`;
-    resultLiPopulation.textContent = `Population: ${population}`;
-    resultLiHabitat.textContent = `Habitat: ${habitat}`;
-    resultLiStatus.textContent = `Conservation status: ${conservationStatus}`;
-
-    if (conservationStatus === "Extinct" || conservationStatus === "Extinct in the wild" || conservationStatus === "Critically endangered") {
-        console.dir(conservationStatus);
-        resultLiStatus.className = "red"
-    } else if (conservationStatus === "Endangered" || conservationStatus === "Vulnerable" || conservationStatus === "Near threatened") {
-        resultLiStatus.className = "yellow"
-    }else{
-        resultLiStatus.className = "green"
-    }
+    // if (conservationStatus === "Extinct" || conservationStatus === "Extinct in the wild" || conservationStatus === "Critically endangered") {
+    //     console.dir(conservationStatus);
+    //     resultLiStatus.className = "red"
+    // } else if (conservationStatus === "Endangered" || conservationStatus === "Vulnerable" || conservationStatus === "Near threatened") {
+    //     resultLiStatus.className = "yellow"
+    // }else{
+    //     resultLiStatus.className = "green"
+    // }
 
 
 
-    resultDiv.appendChild(resultH);
-    resultUl.appendChild(resultLiSciName);
-    resultUl.appendChild(resultLiPopulation);
-    resultUl.appendChild(resultLiHabitat);
-    resultUl.appendChild(resultLiStatus);
-    resultDiv.appendChild(resultUl);
+    // resultDiv.appendChild(resultH);
+    // resultUl.appendChild(resultLiSciName);
+    // resultUl.appendChild(resultLiPopulation);
+    // resultUl.appendChild(resultLiHabitat);
+    // resultUl.appendChild(resultLiStatus);
+    // resultDiv.appendChild(resultUl);
 
-    const listToInputResults = document.querySelector('.list');
-    listToInputResults.appendChild(resultDiv);
-    this.reset();
+    // const listToInputResults = document.querySelector('.list');
+    // listToInputResults.appendChild(resultDiv);
+    // this.reset();
 }
